7b13a92595c992179453f66cd38bfb2eb3beadec
dinokjmm <dino.kjmm@gmail.com>
2025-10-11 17:36:06 -0600

    update: Ajustes finales para despliegue de API

--- models/Product.js
+++ models/Product.js
@@ -6,6 +6,10 @@ const ProductSchema = new mongoose.Schema({
     categoria: { type: String },
     precio_live: { type: Number, required: true },
     precio_local: { type: Number, required: true },
+    categoria: { type: String, required: true }, 
+
+    // ⬅️ AGREGAR ESTE CAMPO DE CANTIDAD/STOCK
+    cantidad: { type: Number, required: true, min: 0 }, 
     estatus: { 
         type: String, 
         required: true, 
